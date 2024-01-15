e3d0e9e4b5d61a1d3de95b13e4ec015c899b17a0
Uziflaona <kobraneses@gmail.com>
2024-01-15 14:38:10 +0900

    learn scripting and instancing

---
+++ scripting/Sprite.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+var speed = 400
+var angular_speed = PI
+
+
+func _ready():
+	var timer = get_node("BlinkingTimer")
+	timer.connect("timeout", self, "_on_timer_timeout")
+
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
+
+
+func _on_timer_timeout():
+	visible = not visible
